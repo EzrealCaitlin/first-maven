0d553d98220ae9b7a37c3f16d9bc69e65d48268c
EzrealCaitlin <949313762@qq.com>
2017-11-08 08:35:14 +0800

    maven学习

---
+++ study-model/src/main/java/com/liukang/study/model/Food.java
@@ -0,0 +1,26 @@
+package com.liukang.study.model;
+
+public class Food {
+	private String name;
+	private int price;
+	private String color;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+}
---
+++ study-model/src/main/java/com/liukang/study/service/StudentManager.java
@@ -0,0 +1,7 @@
+package com.liukang.study.service;
+
+import com.liukang.study.model.Food;
+
+public interface StudentManager {
+	public void eat(Food food);
+}
